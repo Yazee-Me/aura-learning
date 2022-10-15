9c1a156852c1721163ad411eecff56b3abb2ee21
Ionut Badea <20051018+ionut-badea@users.noreply.github.com>
2022-10-15 17:33:50 +0300

    Merge dev branch into main branch (#6)
    * POC (#1)
    
    * image path bugfix and click-outside directive (#3)
    
    Fixed image path and added click-outside directive.
    
    * Added persons to data file (#5)

--- components/Profile.vue
+++ components/Profile.vue
@@ -2,10 +2,12 @@
   <div class="col-12 col-md-6 col-lg-4">
     <NuxtLink :to="redirectTo" class="text-dark">
       <div class="card mb-4 p-3">
-        <img class="card-img align-self-center photo" :src="`/images/${photo}`" :alt="name" />
+        <div class="align-self-center photo">
+          <img class="card-img" :src="imageSrc" :alt="name" />
+        </div>
         <div class="card-body">
           <h3 class="card-title text-center">{{name}}</h3>
-          <p>{{description}}</p>
+          <p>{{shortDescription}}</p>
         </div>
       </div>
     </NuxtLink>
@@ -13,15 +15,15 @@
 </template>
 
 <script lang="ts" setup>
-const { id, name, photo, shortDescription } = defineProps<{
+const { id, name, description } = defineProps<{
   id: string;
   name: string;
-  photo: string;
-  shortDescription: string;
+  description: string[];
 }>();
 
-const description = computed(() => shortDescription.slice(0, 100) + "...");
+const shortDescription = computed(() => description[0].slice(0, 150) + " ...");
 const redirectTo = computed(() => `/profile/${id}`);
+const imageSrc = computed(() => `/images/${name.replace(/\s/g, '_').toLowerCase()}/1.jpg`);
 </script>
 
 <style lang="scss" scoped>
@@ -32,5 +34,12 @@ $photo-size: 100px;
   height: $photo-size;
   border-radius: $photo-size;
   border: 1px solid #000;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 }
 </style>
--- pages/profile/[id].vue
+++ pages/profile/[id].vue
@@ -1,15 +1,60 @@
 <template>
-  <div class="container mt-5">
-    <div class="d-flex flex-column align-items-center">
-      <img class="photo" src="/images/avatar.png" :alt="profile.name" />
+  <div class="container mt-5 d-flex flex-column">
+    <div class="d-flex flex-column align-items-center align-self-center card py-4 px-5">
+      <div class="photo">
+        <img class="border-secondary" :src="`/images/${name}/1.jpg`" :alt="profile.name" />
+      </div>
       <h3 class="mt-5">{{profile.name}}</h3>
     </div>
-    <div class="row">
-      <div class="col-12 col-md-6">
-        {{profile.longDescription}}
+    <div class="row gap-5 d-flex justify-content-center my-5">
+      <div class="col-12 col-lg-5 card p-5">
+        <p v-for="description of profile.description" :key="description">{{description}}</p>
+        <b>Skills:</b>
+        <ul>
+          <li v-for="skill of profile.skills">
+            {{skill}}
+          </li>
+        </ul>
+        <div v-if="profile.qualifications.length > 0">
+          <b>Qualifications:</b>
+          <ul>
+            <li v-for="qualification of profile.qualifications">
+              {{qualification}}
+            </li>
+          </ul>
+        </div>
+        <b>
+          Hourly rate: ${{profile.hourlyRate}}
+        </b>
       </div>
-      <div class="col-12 col-md-6">
+      <div class="col-12 col-lg-5 card p-5">
         <iframe :src="profile.schedulerLink" frameborder="0" width="100%" height="780px"></iframe>
+      </div>
+    </div>
+    <div class="row d-flex flex-row align-items-center justify-content-center">
+      <div class="col-1 d-flex justify-content-end">
+        <button class="btn d-flex justify-content-center" type="button" data-bs-target="#reviews" data-bs-slide="prev">
+          <span class="material-symbols-outlined">
+            arrow_back_ios
+          </span>
+        </button>
+      </div>
+      <div class="col-10 py-5">
+        <div id="reviews" class="carousel slide">
+          <div class="carousel-inner">
+            <div class="carousel-item" :class="{'active': currentReview === profile.reviews.indexOf(review)}"
+              v-for="review of profile.reviews" :key="review">
+              <p class="m-0">{{review}}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-1 d-flex justify-content-start">
+        <button class="btn d-flex justify-content-center" type="button" data-bs-target="#reviews" data-bs-slide="next">
+          <span class="material-symbols-outlined">
+            arrow_forward_ios
+          </span>
+        </button>
       </div>
     </div>
   </div>
@@ -22,6 +67,8 @@ const route = useRoute();
 const id = route.params.id as string;
 
 const profile = computed(() => profiles.find((p) => p.id === id));
+const name = computed(() => profile.value.name.replace(/\s/g, '_').toLowerCase());
+const currentReview = ref(0);
 </script>
 
 <style lang="scss" scoped>
@@ -31,6 +78,13 @@ $photo-size: 300px;
   width: $photo-size;
   height: $photo-size;
   border-radius: $photo-size;
-  border: 1px solid #000;
+  border: 1px solid;
+  overflow: hidden;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
 }
 </style>
